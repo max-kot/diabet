6178b59578b95daad6e1f13b4a18efe44355bae9
max-kot <max.kotlarenko@gmail.com>
2023-12-24 14:14:33 +0300

    added calculation and fixed some bugs

--- js/mathInput.js
+++ js/mathInput.js
@@ -15,6 +15,7 @@ function createMathTools(parent) {
 	for (let key in mathOperations) {
 		let operationButton = document.createElement('button');
 		operationButton.setAttribute('type', 'button');
+		operationButton.classList.add('mathtools__btn');
 		operationButton.innerHTML = mathOperations[key];
 		mathBox.appendChild(operationButton);
 	}
@@ -22,8 +23,57 @@ function createMathTools(parent) {
 	parent.appendChild(mathBox);
 }
 
+function createMathResult(parent) {
+	let mathResult = document.createElement('div');
+	mathResult.classList.add('mathtools__result');
+	mathResult.innerHTML = 0;
+
+	parent.appendChild(mathResult);
+}
+
 let allInputParentEl = document.querySelectorAll('.mathtools');
 
 allInputParentEl.forEach((inputParent) => {
 	createMathTools(inputParent);
+	createMathResult(inputParent);
+	let currentInput = inputParent.querySelector('input');
+
+	currentInput.addEventListener('click', () => {
+		allInputParentEl.forEach((inputParent) => inputParent.classList.remove('active'));
+		inputParent.classList.add('active');
+	})
+
+	currentInput.addEventListener('focus', () => {
+		allInputParentEl.forEach((inputParent) => inputParent.classList.remove('active'));
+
+		inputParent.classList.add('active');
+	})
+
+	
+	let mathResult = inputParent.querySelector('.mathtools__result');
+	currentInput.addEventListener('input', () => {
+		mathResult.innerHTML = currentInput.value ? eval(currentInput.value) : 0;
+	})
+	currentInput.addEventListener('change', () => {
+		mathResult.innerHTML = currentInput.value ? eval(currentInput.value) : 0;
+	})
+	
+	currentInput.addEventListener('blur', () => {
+		allInputParentEl.forEach((inputParent) => inputParent.classList.remove('active'));
+	})
 })
+
+function operationButtonHandler() {
+	let allOperationButton = document.querySelectorAll('.mathtools__btn');
+
+	allOperationButton.forEach((btn) => {
+
+		btn.addEventListener('click', (e) => {
+			const currentInput = e.target.parentElement.previousElementSibling;
+			currentInput.value += btn.innerHTML;
+			currentInput.focus();
+		})
+	})
+}
+
+operationButtonHandler();
